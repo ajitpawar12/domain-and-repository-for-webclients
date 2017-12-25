a91aa5074302ba6703b17c526eba07616364377d
ajit <ajit@idpsolutions.com.au>
2017-12-25 11:24:24 +0530

    Add project files.

---
+++ WebApIDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApIDemo.Startup))]
+namespace WebApIDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
