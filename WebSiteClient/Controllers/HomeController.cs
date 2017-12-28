c430b4d035259605f28593b7f92474b50cb3861d
ajit <ajit@idpsolutions.com.au>
2017-12-28 20:08:10 +0530

    Delete unwanted files

--- WebSiteClient/Controllers/HomeController.cs
+++ WebSiteClient/Controllers/HomeController.cs
@@ -10,24 +10,8 @@ namespace WebSiteClient.Controllers
 {
     public class HomeController : Controller
     {
-        DataContext context=new DataContext();
         public ActionResult Index()
         {
-           
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-           
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
