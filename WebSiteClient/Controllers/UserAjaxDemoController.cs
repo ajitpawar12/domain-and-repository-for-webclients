ec940a6efd71d5b4dd32bf3b3418ba9d4cc07008
ajitpawar12 <ajpawar2012@gmail.com>
2018-02-28 08:55:36 +0530

    Add AjaxDemo

---
+++ WebSiteClient/Controllers/UserAjaxDemoController.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Interfaces;
+using Domain.Models;
+using Domain.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSiteClient.Controllers
+{
+    public class UserAjaxDemoController : Controller
+    {
+        public readonly IDataRepository _Repository;
+        public UserAjaxDemoController()
+        {
+            _Repository = new DataRepository();
+
+        }
+        // GET: UserAjax
+        public ActionResult Index()
+        {
+            var allauser = _Repository.AllUsers();
+            return View(allauser);
+        }
+        [HttpPost]
+        public JsonResult Create(User user)
+        {
+            _Repository.AddUser(user);
+            return Json("");
+        }
+    }
+}
