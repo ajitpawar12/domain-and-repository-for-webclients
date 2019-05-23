16748a96bf7a286b01b64946258c8e05d5d5a81d
Ajit Pawar <ajpawar2012@gmail.com>
2019-05-23 17:14:17 +0530

    Add AngularJs demo and fix CORS error for webapi calls

---
+++ WebSiteClient/Controllers/UserAngularJSDemoController.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Interfaces;
+using Domain.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSiteClient.Controllers
+{
+    public class UserAngularJSDemoController : Controller
+    {
+        public readonly IDataRepository _Repository;
+        public UserAngularJSDemoController()
+        {
+            _Repository = new DataRepository();
+        }
+        // GET: UserAngularJSDemo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
